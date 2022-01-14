38062cdc66959317222789967f9c7d87a6c6e5a1
Vitor Quadros <viitorquadros@hotmail.com>
2022-01-14 11:05:33 -0300

    feat: create references to time picker, interval input and notify button

--- app/src/main/java/dev/vitorquadros/drinkwater/MainActivity.java
+++ app/src/main/java/dev/vitorquadros/drinkwater/MainActivity.java
@@ -3,12 +3,25 @@ package dev.vitorquadros.drinkwater;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnNotify;
+    private EditText editMinutes;
+    private TimePicker timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNotify = findViewById(R.id.btn_notify);
+        editMinutes = findViewById(R.id.edit_txt_number_interval);
+        timePicker = findViewById(R.id.time_picker);
+
+        timePicker.setIs24HourView(true);
     }
 }
